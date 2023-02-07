14cff077471cf041458485ef805982aadc0c9ad5
user <user@AMR2023.myguest.virtualbox.org>
2023-02-07 23:54:34 +0300

    added code

--- homework1/hagen_control_strategy.py
+++ homework1/hagen_control_strategy.py
@@ -1,3 +1,6 @@
+# GitHubLink
+# https://github.com/ipipos56/AutonomousMobileRoboticsHW
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -30,6 +33,10 @@ class ControlStrategy(Node):
         self.t = np.arange(0, duration, self.Ts)  # Simulation time
         self.end_controller = False
         self.timer = self.create_timer(self.Ts, self.timer_callback)
+        self.v = 0
+        self.w = 0
+        self.q_info = []
+        self.sim_q_info = []
 
     def euler_from_quaternion(self, quaternion):
         """
@@ -61,131 +68,32 @@ class ControlStrategy(Node):
     def stop_vehicle(self, ):
         self.send_vel(0.0, 0.0)
 
-    def wrap_to_pi(self, x):
-        x = np.array([x])
-        xwrap = np.remainder(x, 2 * np.pi)
-        mask = np.abs(xwrap) > np.pi
-        xwrap[mask] -= 2 * np.pi * np.sign(xwrap[mask])
-        return xwrap[0]
-
-    def diff_drive_init(self, duration=10):
+    def diff_drive_init(self, v=3, w=1, duration=4):
+        self.q_info = []
+        self.sim_q_info = []
         self.duration = duration
-        self.wL = 12  # Left wheel velocity
-        self.wR = 12  # Right wheel velocity
+        self.v = v
+        self.w = w
         self.time_utilized = 0.0
 
-    def inter_point_diff_drive_init(self, duration=10, r_distance=1.3
-                                    , refPose=np.array([3, 6, 0]), k_p=0.5, k_w=0.7, dmin=0.7):
+    def timer_callback(self):
+        self.time_utilized += self.Ts
 
-        self.duration = duration
-        self.r_distance = r_distance
-        self.refPose = refPose
-        self.k_p = k_p
-        self.k_w = k_w
-        self.dmin = dmin
-        self.time_utilized = 0.0
-        self.xT = self.refPose[0] - self.r_distance * np.cos(self.refPose[2])
-        self.yT = self.refPose[1] - self.r_distance * np.sin(self.refPose[2])
-        self.state = 0
+        if self.time_utilized < self.duration:
+            print("send vel" + str(self.v) + " " + str(self.w))
+            self.send_vel(self.v, self.w)
+            return
 
-    def inter_direction_diff_drive_init(self, duration=10, r_distance=1.3
-                                        , refPose=np.array([3, 6, 0]), k_p=0.5, k_w=0.7, dmin=0.7):
-        self.duration = duration
-        self.r_distance = r_distance
-        self.refPose = refPose
-        self.k_p = k_p
-        self.k_w = k_w
-        self.dmin = dmin
-        self.time_utilized = 0.0
+        self.sim_q_info.append(self.q)
+        self.stop_vehicle()
+        self.end_controller = True
+        print("i have finished callback")
 
-    def inter_direction_diff_drive(self, ):
-        if (self.q is not None):
-            if (self.duration < self.time_utilized):
-                print("End of simulation")
-                self.send_vel(0.0, 0.0)
-                self.end_controller = True
+        time.sleep(1)
+        os.system('ros2 service call /reset_simulation std_srvs/srv/Empty "{}"')
+        time.sleep(1)
 
-            self.D = np.sqrt((self.q[0] - self.refPose[0]) ** 2
-                             + (self.q[1] - self.refPose[1]) ** 2)
-
-            if (self.D < self.dmin):
-                print("Reach to the goal pose")
-                self.send_vel(0.0, 0.0)
-                self.end_controller = True
-
-            beta = np.arctan(self.r_distance / self.D)
-            phiR = np.arctan2(self.refPose[1] - self.q[1], self.refPose[0] - self.q[0])
-            alpha = self.wrap_to_pi(phiR - self.refPose[2])
-
-            if (alpha < 0):
-                beta = -beta
-            ##Controller
-            if (np.abs(alpha) < np.abs(beta)):
-                ePhi = self.wrap_to_pi(phiR - self.q[2] + alpha)
-            else:
-                ePhi = self.wrap_to_pi(phiR - self.q[2] + beta)
-
-            v = self.k_p * self.D
-            w = self.k_w * ePhi
-            print("Distance to the goal: ", self.D)
-            dq = np.array([v * np.cos(self.q[2] + self.Ts * w / 2)
-                              , v * np.sin(self.q[2] + self.Ts * w / 2), w])
-            self.q = self.q + self.Ts * dq  # Integration
-            self.q[2] = self.wrap_to_pi(self.q[2])  # Map orientation angle to [-pi, pi]
-            self.send_vel(v, w)
-            self.time_utilized = self.time_utilized + self.Ts
-
-    def inter_point_diff_drive(self, ):
-        if (self.q is not None):
-            if (self.duration < self.time_utilized):
-                self.stop_vehicle()
-                print("End of simulation")
-                self.end_controller = True
-
-            self.D = np.sqrt((self.q[0] - self.refPose[0]) ** 2 + (self.q[1] - self.refPose[1]) ** 2)
-
-            if (self.D < self.dmin):
-                self.stop_vehicle()
-                print("Reach to the goal pose")
-                self.end_controller = True
-
-            if self.state == 0:
-                d = np.sqrt((self.yT - self.q[1]) ** 2 + (self.xT - self.q[0]) ** 2)
-                if (d < self.dmin):
-                    self.state = 1
-                self.phiT = np.arctan2(self.yT - self.q[1], self.xT - self.q[0])
-                self.ePhi = self.phiT - self.q[2]
-            else:
-                self.ePhi = self.refPose[2] - self.q[2]
-
-            v = self.k_p * self.D
-            w = self.k_w * self.ePhi
-            print("Distance to the goal: ", self.D)
-            dq = np.array([v * np.cos(self.q[2] + self.Ts * w / 2), v * np.sin(self.q[2] + self.Ts * w / 2), w])
-            self.q = self.q + self.Ts * dq  # Integration
-            self.q[2] = self.wrap_to_pi(self.q[2])  # Map orientation angle to [-pi, pi]
-            self.send_vel(v, w)
-            self.time_utilized = self.time_utilized + self.Ts
-
-    def perform_action_diff_drive_one_step(self):
-        v = self.r / 2 * (self.wR + self.wL)  # Robot velocity
-        w = self.r / self.L * (self.wR - self.wL)  # Robot angular velocity
-        dq = np.array([v * np.cos(self.q[2] + self.Ts * w / 2), v * np.sin(self.q[2] + self.Ts * w / 2), w])
-        self.q = self.q + self.Ts * dq  # Integration
-        self.q[2] = self.wrap_to_pi(self.q[2])  # Map orientation angle to [-pi, pi]
-        self.send_vel(v, w)
-        self.time_utilized = self.time_utilized + self.Ts
-
-    def const_param_move(self, lin_ang_vel):
-
-
-
-    def const_param_one_step(self, lin_ang_vel):
-
-
-
-    def timer_callback(self, ):
-        self.const_param_one_step()
+        # self.const_param_one_step()
         # self.perform_action_diff_drive_one_step()
         # self.inter_direction_diff_drive()
         # self.inter_point_diff_drive()
@@ -193,14 +101,16 @@ class ControlStrategy(Node):
 
     def set_pose(self, msg):
         _, _, yaw = self.euler_from_quaternion(msg.pose.pose.orientation)
+        self.q_info.append(np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, yaw]))
         if (self.set_q_init is None):
             self.set_q_init = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, yaw])
             self.q = self.set_q_init
+            self.sim_q_info.append(self.set_q_init)
 
     def send_vel(self, v, w):
         msg = Twist()
-        msg.linear.x = v
-        msg.angular.z = w
+        msg.linear.x = float(v)
+        msg.angular.z = float(w)
         self.publisher_.publish(msg)
 
 
@@ -210,29 +120,37 @@ def simulate_all(args, ang_lin_vel):
     # control_strategy.diff_drive_init()
     # control_strategy.inter_point_diff_drive_init()
     # control_strategy.inter_direction_diff_drive_init()
-    control_strategy.const_param_move(ang_lin_vel)
+    control_strategy.diff_drive_init(v=ang_lin_vel[0], w=ang_lin_vel[1])
     while control_strategy.end_controller is False and rclpy.ok():
         try:
             rclpy.spin_once(control_strategy)
         except KeyboardInterrupt:
             break
 
+    # info = control_strategy.q_info
+    # sim_info = control_strategy.sims_q_info
     info = control_strategy.q_info
-    sim_info = control_strategy.sims_q_info
+    sim_info = control_strategy.sim_q_info
 
-    control_strategy.stop_vehicle()
-    control_strategy.stop_vehicle()
-    control_strategy.stop_vehicle()
-    control_strategy.stop_vehicle()
+    # control_strategy.stop_vehicle()
+    # control_strategy.stop_vehicle()
+    # control_strategy.stop_vehicle()
+    # control_strategy.stop_vehicle()
 
-    time.sleep(1)
-    os.system('ros2 service call /reset_simulation std_srvs/srv/Empty "{}"')
-    time.sleep(1)
 
     control_strategy.destroy_node()
     rclpy.shutdown()
 
     return info, sim_info
+
+
+L = 0.08
+
+
+def vlvr_to_vw(vl, vr):
+    v = (vr + vl) / 2
+    w = (vr - vl) / L
+    return v, w
 
 
 def main(args=None):
@@ -244,14 +162,31 @@ def main(args=None):
 
     info = []
     sims = []
-    analyt = []
 
     for ang_lin_vel in ang_lin_vel_args:
-        q_info, sims_info = simulate_all(args=args, delta_t=delta_t, r=r, L=L, duration=duration, velocity=velocity)
-        dumps.append(q_dump)
-        ref.append(ref_dump)
-        analytical.append(analytical_solution(velocity, delta_t, duration, r, L))
+        q_info, sims_info = simulate_all(args, ang_lin_vel)
+        info.append(q_info)
+        sims.append(sims_info)
 
+    q_info, sims_info = simulate_all(args, vlvr_to_vw(*left_right_vel))
+    info.append(q_info)
+    sims.append(sims_info)
+
+    print(info)
+    print("______________________")
+    print(sims)
+
+
+    with open('info.txt', 'w') as outfile:
+        for t in info:
+            for i in t:
+                outfile.write(f'{i[0]} {i[1]} ')
+            outfile.write('\n')
+
+    with open('sims.txt', 'w') as outfile:
+        for t in sims:
+            for i in t:
+                outfile.write(f'{i[0]} {i[1]} \n')
 
 if __name__ == '__main__':
     main()
